a55f9dcb83b07cc58e218efcc73760de296b5d7e
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2024-10-11 22:28:13 +0500

    feat(Medium): Fizz Buzz

---
+++ Level/3. Medium/Fizz Buzz.py
@@ -0,0 +1,11 @@
+def fizz_buzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return str(num)
+
+print(fizz_buzz(98))
